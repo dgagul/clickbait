e3c702c8ba74d471d08188dc955b6f3e6f421322
dgagul <dario.gagulic@gmail.com>
2022-10-17 00:03:22 +0200

    added statistics to popup

--- client/popup/popup.js
+++ client/popup/popup.js
@@ -4,7 +4,9 @@ var headlines = document.getElementsByClassName("cd__headline-text");
 
 
 const localhost = "http://127.0.0.1:5000/";
-const URL = "https://regres.in/api/users";
+var statistics = document.getElementById("statistics");
+var info = document.getElementById("info");
+
 
 const sendHttpRequest = (method, url, data) => {
     const promise = new Promise((resolve, reject) => {
@@ -18,17 +20,14 @@ const sendHttpRequest = (method, url, data) => {
                 resolve(xhr.response);
             }
         };
-        // xhr.onerror = () => {
-        //     reject("Something went wrong!");
-        // };
+        xhr.onerror = () => {
+            reject("Something went wrong!");
+        };
 
 
         xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
         xhr.setRequestHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
         xhr.setRequestHeader('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
-        if (data) {
-            xhr.setRequestHeader('Content-Type', 'application/json');
-        }
         xhr.send(JSON.stringify(data));
     });
     return promise;
@@ -36,20 +35,36 @@ const sendHttpRequest = (method, url, data) => {
 
 const getClickbait = () => {
     sendHttpRequest("GET", localhost).then(responseData => {
-        console.log(responseData);
+        var websites = Object.keys(responseData)
+        for(var website in websites){
+            var nrClickbait = responseData[websites[website]][0];
+            var nrTotal = responseData[websites[website]][1];
+            // Create Statistics Label
+            var label = document.createElement("label");
+            label.setAttribute("for","file");
+            label.insertAdjacentText('beforeend',`The percentage of clickbait on ${websites[website]} is:`);
+            // Create Statistics Progress Bar
+            var div = document.createElement("div");
+            var p = document.createElement("p");
+            p.insertAdjacentText('beforeend',`${Math.round(nrClickbait/nrTotal*100)}%`);
+            var progress = document.createElement("progress");
+            progress.setAttribute("id","file");
+            progress.setAttribute("value",`${nrClickbait/nrTotal*100}`);
+            progress.setAttribute("max","100");
+            // Insert Label and Progress Bar
+            div.appendChild(label);
+            div.appendChild(p);
+            div.appendChild(progress);
+            div.setAttribute("class","webstat");
+            statistics.appendChild(div);
+        };
+        if (websites.length > 0) {
+            info.classList.toggle('active');
+            statistics.classList.toggle('active');
+        };
     });
-    // .catch(err => {
-    //     console.log(err);
-    // });
-};
-
-const sendRequest = () => {
-    for(i=0; i< headlines.length; i++) {
-        sendHttpRequest("POST", localhost, {headline: headlines[i].textContent}).then(responseData => {
-            console.log(responseData);
-        });
-    };
 };
 
 
-getCbtBtn.addEventListener('click', sendRequest); 
+
+getClickbait();
